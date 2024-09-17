54ac7cba4cbc116a947b5cc1afa9bada40d1ead2
Asad Basha <145152174+Asadcodeing@users.noreply.github.com>
2024-09-17 09:47:41 +0000

    comparisions

---
+++ Basics/comparisions.js
@@ -0,0 +1,20 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log("02" < 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+// Here, null will be converted into the 0 and >,==,>=,<= have a different syntax
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+
+
+// === Whenever you want to compare use "===" not "=="
+// avoide up ones comparision
+
+console.log("2" === 2);
