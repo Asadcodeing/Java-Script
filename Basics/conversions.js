b55e25966e1f565397be52bcbb8ff721fb13cdf1
Asad Basha <145152174+Asadcodeing@users.noreply.github.com>
2024-09-17 09:09:05 +0000

    conversions of datatypes

---
+++ Basics/conversions.js
@@ -0,0 +1,29 @@
+let score = "slk"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let a = Number(score)
+// console.log(typeof a);
+// console.log(a);
+
+// "33" => 33
+// "33aed" => NaN
+// true => 1; false => 0
+
+let isLoggin = "33"
+
+let b = Boolean(isLoggin)
+// console.log(b);
+
+// 1 => true; 0 => false
+// "" => false
+// "asad" => true
+// "24" => true
+
+let someNumber = 24
+
+let c = String(someNumber)
+console.log(typeof c);
+
+console.log(c);
