b8408cbe2ffd4a866bc54311b7b36260869d3c4a
Asad Basha <145152174+Asadcodeing@users.noreply.github.com>
2024-09-17 09:35:38 +0000

    string conversions

---
+++ Basics/stringconversions.js
@@ -0,0 +1,32 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = "Asad"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log(1 + "2");
+// console.log("1" + 2+ 2);
+// console.log(1 + 2+ "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+// console.log(gameCounter);
+
+let x = 4
+// let y = x++
+let z = ++x
+// console.log(`x:${x}, y:${y}`);
+console.log(`x:${x}, z:${z}`);
+
+
+// If we use pre-fix operation then both values will be increase.
+// If we use post-fix operation then x will be increase and y will takes the x value.
